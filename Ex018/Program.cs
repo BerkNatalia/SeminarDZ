eb3d89b949d9e6be4d0d97fb9154ec69482ab9b4
Natalia <natberk.77@mail.ru>
2022-08-24 13:25:52 +0600

    Ex018

---
+++ Ex018/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] masNum = new int[m];
+InNum(m);
+void InNum(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    masNum[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison(int[] masNum)
+{
+  int count = 0;
+  for (int i = 0; i < masNum.Length; i++)
+  {
+    if(masNum[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+Console.WriteLine($"Введено чисел больше 0: {Comparison(masNum)} ");
