01c208583a564a37b5f41d4642263ee944b81ddd
Natalia <natberk.77@mail.ru>
2022-08-24 11:57:12 +0600

    Ex015

---
+++ Ex015/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введи количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()); 
+int[] numbers = new int[n];
+
+void MasArray(int[] array, int min, int max)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i<array.Length; i++ )
+    {
+    Console.Write(array[i] + " ");
+    }
+  Console.WriteLine();
+}
+
+int Quantity(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ )
+    {
+      if (array[i] % 2 == 0)
+      {
+      quantity++;
+      }
+    }
+  return quantity;
+}
+
+MasArray(numbers, 100, 1000);
+WriteArray(numbers);
+Console.WriteLine();
+
+int quantity = Quantity(numbers);
+Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
